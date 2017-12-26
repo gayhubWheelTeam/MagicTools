b996539d6131a0e44175852622f1146a0a9b3974
git0x4c <969771700@qq.com>
2017-12-26 23:31:14 +0800

    增加判断微信浏览器

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 魔法工具库
+#### 使用方法：
+```
+<script src=“index.js”>
+var mt = new MagicToools();
+mt.xxx();
+```
+
+#### 1.判断微信浏览器
+`var isWX = mt.isWeiXin();`
+```
+说明：返回true说明是微信浏览器，否则返回false。
+```
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ * @name 魔法师工具箱
  * @author Magic.H
  * @version 1.0.0
  * @date 2017.12.15
@@ -6,8 +7,19 @@
 
 function MagicTools() {
 	return {
-		
+		/**
+		 * 判断微信浏览器
+		 * @param null
+		 * @return boolean
+		*/
+		isWeiXin: function () {
+			var ua = window.navigator.userAgent.toLowerCase(); // 读取浏览器ua
+			var exp = '/MicroMessenger/i'; // 微信浏览器正则表达式 忽略大小写
+			if (ua.match(exp) == 'micromessenger') { // 判断是否为微信浏览器
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 }
-
-var mt = new MagicTools();
