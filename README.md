44a36bb788857acc933f1bb84ca2acc15f35f7e3
git0x4c <969771700@qq.com>
2018-02-27 00:24:36 +0800

    修改md文档 删除index.html

--- README.md
+++ README.md
@@ -1,20 +1,66 @@
-# 魔法工具库
+
 #### 使用方法：
 ```
-<script src=“index.js”>
-var mt = new MagicToools();
-mt.xxx();
+<script src="index.js">
+MagicTools.xxx()
 ```
 
-#### 1.判断浏览器
-`var isBrower = mt.isBrower(type);`
 
-说明：
-+ 可选参数：
-    + type = 'wx'
-    + type = 'chrome'
-    + type = 'safari'
-    + type = 'opera'
-+ 返回true说明是对应浏览器，否则返回false。
+### 1.判断浏览器
+```
+使用方法：isBrower(type);
+传递参数：type -> 参数名称(参见下表)
+返回数据：true or false
+```
+| 参数名称 | 说明 | 参数格式 |
+| :-: | :-: | :-: |
+| wx | 微信浏览器 | string |
+| chrome | 谷歌浏览器 | string |
+| safari | 苹果浏览器 | string |
+| opera | 欧朋浏览器 | string |
+
+### 2.判断是否为数组
+```
+使用方法：isArray(array);
+传递参数：array -> 待检测参数
+返回数据：true or false
+```
+
+### 3.数组去重
+```
+使用方法：removeDuplicatedItem(array);
+传递参数：array -> 待去重数组
+返回数组：去重后的数组
+```
+### 4.获取url地址里指定参数
+```
+使用方法：getUrlParam(name);
+传递参数：name -> 要取的参数名称
+如：www.xxx.com?code="abc"------MagicTools.getUrlParam('code')
+返回数据：匹配到的参数值 or null. 上例中返回'abc'
+```
+### 5.浮点数取整 (支持负数)
+```
+使用方法：floatToInt(float_num);
+传递参数：float_num -> 浮点数 支持负数 如 100.99 、-100.33
+返回数据：取整数（负数则保留负号）
+```
+### 6.生成4~6位数字验证码
+```
+使用方法：validCode(num);
+传递参数：num -> 验证码位数：4,5,6
+返回数据：数字验证码
+```
+
+### 7.随机生成16进制颜色代码
+```
+使用方法：createColor();
+返回数据：16进制颜色编码 如#ff0000
+```
+### 8. 获取url查询参数并转为json格式
+```
+使用方法：getUrlParamToObj();
+返回数据：json数据 or null
+```
 
 
--- index.js
+++ index.js
@@ -1,147 +1,146 @@
 /**
- * @name 魔法工具箱
- * @author Magic.H
+ * @name 常用js代码小轮子
+ * @author Magic Liang
  * @version 1.0.0
  * @date 2017.12.15
  */
 
-function MagicTools() {
-    return {
-        /**
-         * 浏览器列表
-         * @param browerName 浏览器名称
-         * @return {object} 返回一组含有浏览器名称以及正则匹配得到的浏览器名称
-         */
-        _Borwers: function (browerName) {
-            browerName = browerName.toLowerCase();
-            let ua = window.navigator.userAgent.toLowerCase(); // 获取浏览器ua
-            let exp = {
-                type: '',
-                msg: ''
-            };
-            switch (browerName) {
-                case 'wx': // 微信
-                    exp.type = ua.match(/MicroMessenger/i);
-                    exp.msg = 'micromessenger';
-                    break;
-                case 'chrome': // 谷歌
-                    exp.type = ua.match(/Chrome/i);
-                    exp.msg = 'chrome';
-                    break;
-                case 'safari': // 苹果
-                    exp.type = ua.match(/Safari/i);
-                    exp.msg = 'safari';
-                    break;
-                case 'opera': // 欧朋
-                    exp.type = ua.match(/Opera/i);
-                    exp.msg = 'opera';
-                    break;
-                default:
-                    console.error("Can't find this browser!");
-                    break;
-            }
-            return exp;
-        },
-        /**
-         * 判断浏览器
-         * @param browerName 浏览器名称
-         * @return boolean
-         */
-        isBrower: function (browerName) {
-            let exp = this._Borwers(browerName);
-            return (exp.type == exp.msg && exp.type !== '' && exp.msg !== '') ? true : false;
-        },
-        /**
-         * 判断是否为数组
-         * @param o 待检测参数
-         * @return boolean
-         */
-        isArray: function (o) {
-            return Object.prototype.toString.call(o) == '[object Array]';
-        },
-        /**
-         * 数组去重 es6
-         * @param array 原始数组
-         * @return [arr] 去重后数组
-         */
-        removeDuplicatedItem: function (array) {
-            // 判断是否为数组
-            if (!this.isArray(array)) {
-                console.error('parameter error');
-                return;
-            }
-            if (array.length == 0) {
-                return new Array();
-            }
-            let ret = [...new Set(array)];
-            return ret;
-        },
-        /*
-        * 获取URL地址里的参数
-        * @param name 需要获取到的参数
-        * 如：www.xxx.com?code="abc"------getUrlParam('code')--->'abc'
-        * */
-        getUrlParam: function (name) {
-            let isParam = window.location.hash.split('?')[1]
-            if (isParam) {
-                let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-                let param = isParam.match(reg);
-                if (param != null) return decodeURIComponent(param[2]);
-                return null;
-            }
-        },
-        /**
-         *  浮点数取整
-         *  @param float_num 传递的小数 如123.456
-         *  @returns  int_num 整数 如123
-        */
-        floatToInt: function (float_num) {
-            let temp = float_num;
-            // 先把负数变成正数
-            if (float_num < 0) {
-                temp = Math.abs(float_num);
-            }
-            return Math.floor(temp);
-        },
-        /**
-         * 生成4~6位数字验证码
-         * @param num 验证码位数
-         * @returns code 验证码
-        */
-        validCode: function (num) {
-            if (num < 4 || num > 6 || num == null) {
-                console.error('please enter the number between 4 and 6');
-            } else {
-                return ('000000' + Math.floor(Math.random() * 999999)).slice(-parseInt(num));
-            }
-            
-        },
-        /**
-         * 随机生成16进制颜色代码
-         * @param null
-         * @returns color_code 颜色代码
-        */
-        createColor: function () {
-            return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
-        },
-        /**
-         * 获取url查询参数并转为json格式 es5
-         * @param  
-         * @returns obj json对象
-        */
-        getUrlParamToObj: function () {
-            let isParam = window.location.hash.split('?')[1];
-            if (isParam) {
-                let obj = {};
-                isParam.split('&').forEach(function (item) {
-                    (function (kv) {
-                        obj[kv[0]] = kv[1];
-                    })(item.split('='));
-                })
-                return obj;
-            } else {
-                return null;
-            }
+let MagicTools = {
+    /**
+     * 浏览器列表
+     * @param browerName 浏览器名称
+     * @return {object} 返回一组含有浏览器名称以及正则匹配得到的浏览器名称
+     */
+    _Borwers: function (browerName) {
+        browerName = browerName.toLowerCase();
+        let ua = window.navigator.userAgent.toLowerCase(); // 获取浏览器ua
+        let exp = {
+            type: '',
+            msg: ''
+        };
+        switch (browerName) {
+            case 'wx': // 微信
+                exp.type = ua.match(/MicroMessenger/i);
+                exp.msg = 'micromessenger';
+                break;
+            case 'chrome': // 谷歌
+                exp.type = ua.match(/Chrome/i);
+                exp.msg = 'chrome';
+                break;
+            case 'safari': // 苹果
+                exp.type = ua.match(/Safari/i);
+                exp.msg = 'safari';
+                break;
+            case 'opera': // 欧朋
+                exp.type = ua.match(/Opera/i);
+                exp.msg = 'opera';
+                break;
+            default:
+                console.error("Can't find this browser!");
+                break;
+        }
+        return exp;
+    },
+    /**
+     * 判断浏览器
+     * @param browerName 浏览器名称
+     * @return boolean
+     */
+    isBrower: function (browerName) {
+        let exp = this._Borwers(browerName);
+        return (exp.type == exp.msg && exp.type !== '' && exp.msg !== '') ? true : false;
+    },
+    /**
+     * 判断是否为数组
+     * @param o 待检测参数
+     * @return boolean
+     */
+    isArray: function (o) {
+        return Object.prototype.toString.call(o) == '[object Array]';
+    },
+    /**
+     * 数组去重 es6
+     * @param array 原始数组
+     * @return [arr] 去重后数组
+     */
+    removeDuplicatedItem: function (array) {
+        // 判断是否为数组
+        if (!this.isArray(array)) {
+            console.error('parameter error');
+            return;
+        }
+        if (array.length == 0) {
+            return new Array();
+        }
+        let ret = [...new Set(array)];
+        return ret;
+    },
+    /*
+    * 获取URL地址里的指定参数
+    * @param name 需要获取到的参数
+    * 如：www.xxx.com?code="abc"------getUrlParam('code')--->'abc'
+    * */
+    getUrlParam: function (name) {
+        let isParam = window.location.hash.split('?')[1]
+        if (isParam) {
+            let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            let param = isParam.match(reg);
+            if (param != null) return decodeURIComponent(param[2]);
+            return null;
+        }
+    },
+    /**
+     *  浮点数取整
+     *  @param float_num 传递的小数 如123.456
+     *  @returns  int_num 整数 如123
+    */
+    floatToInt: function (float_num) {
+        let temp = float_num;
+        // 如果是负数
+        if (float_num < 0) {
+            temp = Math.abs(float_num);
+            return '-' + Math.floor(temp);
+        }
+        return Math.floor(temp);
+    },
+    /**
+     * 生成4~6位数字验证码
+     * @param num 验证码位数
+     * @returns code 验证码
+    */
+    validCode: function (num) {
+        if (num < 4 || num > 6 || num == null) {
+            console.error('please enter the number between 4 and 6');
+        } else {
+            return ('000000' + Math.floor(Math.random() * 999999)).slice(-parseInt(num));
+        }
+        
+    },
+    /**
+     * 随机生成16进制颜色代码
+     * @param null
+     * @returns color_code 颜色代码
+    */
+    createColor: function () {
+        return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+    },
+    /**
+     * 获取url查询参数并转为json格式 es5
+     * @param  
+     * @returns obj json对象
+    */
+    getUrlParamToObj: function () {
+        let isParam = window.location.hash.split('?')[1];
+        if (isParam) {
+            let obj = {};
+            isParam.split('&').forEach(function (item) {
+                (function (kv) {
+                    obj[kv[0]] = kv[1];
+                })(item.split('='));
+            })
+            return obj;
+        } else {
+            return null;
         }
     }
 }
